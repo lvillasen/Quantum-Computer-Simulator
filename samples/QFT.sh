4540b0153e3010a82e0d6bb9ae3b0289a2df4165
Luis Villaseñor <lvillasen@gmail.com>
2017-01-03 22:05:27 -0600

    Update QFT.sh

--- samples/QFT.sh
+++ samples/QFT.sh
@@ -2,9 +2,10 @@
 # Bash script to generate QFT examples for any number of qubits
 #Autor: Luis Villaseñor
 #Date: 1/1/2017 
-N=20
+N=8
 echo "h q[0:$((N-1))];";echo
-for i in $(seq 0 $((N-1))); do
+echo "h q[0];";echo
+for i in $(seq 1 $((N-1))); do
 	for j in $(seq 0 $((i-1))); do
 		echo "csk q[$i], q[$j], $(( 2 ** $((i-j)) ));"
 	done
