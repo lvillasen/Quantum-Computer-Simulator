dc62bc978f511cc06b2d8da141f0a3a49cbe13b7
Luis Villaseñor <lvillasen@gmail.com>
2017-01-19 08:35:57 -0600

    updated QFT.sh

--- samples/QFT.sh
+++ samples/QFT.sh
@@ -2,14 +2,15 @@
 # Bash script to generate QFT examples for any number of qubits
 #Autor: Luis Villaseñor
 #Date: 1/1/2017 
-# Usage: bash samples/QFT.sh > samples/example_IQFT.txt
+# Usage: bash samples/QFT.sh > samples/example_QFT.txt
 # Usage: python QCSim.py samples/example_QFT.txt
-N=8
-echo "h q[0:$((N-1))];";echo
+n = 5
+echo "QFT code starts here"
+echo "reverse;";echo
 echo "h q[0];";echo
-for i in $(seq 1 $((N-1))); do
+for i in $(seq 1 $((n-1))); do
 	for j in $(seq 0 $((i-1))); do
 		echo "csk q[$i], q[$j], $(( 2 ** $((i-j)) ));"
 	done
-	echo "h q[$i];";echo;
+	echo "h q[$i];";echo "QFT code ends here";
 done
