5733a4d5529bd1903bee8834f6f7fe0b048e919f
Luis Villaseñor <lvillasen@gmail.com>
2017-02-04 09:06:21 -0600

    Updated QFT.sh

--- samples/QFT.sh
+++ samples/QFT.sh
@@ -4,7 +4,7 @@
 #Date: 1/1/2017 
 # Usage: bash samples/QFT.sh > samples/example_QFT.txt
 # Usage: python QCSim.py samples/example_QFT.txt
-n = 5
+n=15
 echo "QFT code starts here"
 echo "reverse;";echo
 echo "h q[0];";echo
@@ -12,5 +12,6 @@ for i in $(seq 1 $((n-1))); do
 	for j in $(seq 0 $((i-1))); do
 		echo "csk q[$i], q[$j], $(( 2 ** $((i-j)) ));"
 	done
-	echo "h q[$i];";echo "QFT code ends here";
+	echo "h q[$i];";echo
 done
+echo "QFT code ends here";
